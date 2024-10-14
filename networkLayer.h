eb4a8e08a2aaa159b5a0f87b81755128ad352b09
karla2prado5 <117671730+karla2prado5@users.noreply.github.com>
2024-10-14 13:57:29 -0600

    Update networkLayer.h
    updated network layer header with some changes to the structure and necessary functions

--- networkLayer.h
+++ networkLayer.h
@@ -4,28 +4,32 @@
 #include <stdint.h>
 #include "link_layer.h"
 
-#define IP_ADDR_LEN 4
-#define MAX_PACKET_SIZE 256
-#define MAX_ROUTING_TABLE_ENTRIES 10
+#define MAX_ADDRESS 255          //max value for 1-byte addresses
+#define MAX_PACKET_SIZE 255      //max packet size for data
+#define MAX_ROUTING_TABLE_ENTRIES 10 //max routing table entries
 
-typedef struct {
-   uint8_t src_ip[IP_ADDR_LEN];
-   uint8_t dest_ip[IP_ADDR_LEN];
-   uint8_t data[MAX_PACKET_SIZE];
-   uint8_t data_len;
-} NetworkPacket;
-
-typedef struct {
-   uint8_t dest_ip[IP_ADDR_LEN];
-   int channel;
-} RoutingTableEntry;
-
-// Network layer initialization function
+//network layer functions
+/**
+ * @brief initialize network layer
+ */
 void network_layer_init();
-void send_packet(uint8_t* dest_ip, uint8_t* data, uint8_t len);
+
+/**
+ * @brief initialize link layer, set up GPIOs and callbacks
+ * @param dest_addr the address we want to send to
+ * @param data pointer to the data being transmitted 
+ * @param len the length of the data being transmitted 
+ */
+void send_packet(uint8_t dest_addr, uint8_t* data, uint8_t len);
+
+/**
+ * @brief callback function to handle incoming messages from the link layer
+ * @param msg pointer to the message recieved 
+ * @param ch the channel the message has been received on 
+ */
 void receive_packet(uint8_t* msg, int ch);
 
-#endif
+#endif // NETWORK_LAYER_H
 
 
 
